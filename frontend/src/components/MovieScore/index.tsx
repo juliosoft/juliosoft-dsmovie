e1f82cf441a2ca8d4d2b5c30ceab2b6183f72cc9
JulioBicharra <jcmail.am@gmail.com>
2022-01-22 11:01:28 -0300

    paginação e busca na api

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,18 @@
 import "./styles.scss";
 import MovieStars from "components/MovieStars";
 
-function MovieScore() {
-  const count = 18;
-  const score = 3.5;
+type Props = {
+  score: number;
+  count: number;
+};
+
+function MovieScore({ score, count }: Props) {
   return (
     <div className="dsmovie-sScore-container">
       <p className="dsmovie-score-value">
         {score > 0 ? score.toFixed(1) : "-"}
       </p>
-      <MovieStars />
+      <MovieStars score={score} />
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
